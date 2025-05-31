e15a1938d41b5ee982f203cbebb7a2ae51698bd2
bharat <bharatkhatwani06@gmail.com>
2025-05-31 19:05:36 +0530

    "path Change "

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login.jsx';
+import Login from './Components/Login.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import  Signup  from './Components/Signup.jsx';
 import Home from './Components/Home.jsx';
