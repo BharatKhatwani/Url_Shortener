932863919897d3d6b26fec68a5c8dc85b956915d
bharat <bharatkhatwani06@gmail.com>
2025-06-01 16:16:25 +0530

    "REPLACE KEYWORD ADDED "

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import  Signup  from './Components/Signup.jsx';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Signup from './Components/Signup.jsx';
 import Home from './Components/Home.jsx';
 import NotFound from './Components/NOTFOUND.jsx';
 import PrivateRoute from './Components/PrivateRoute.jsx';
@@ -11,15 +11,15 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path = '/login' element = {<Login/>} />
-        <Route path = '/signup' element = {<Signup/>} />
-        <Route  path='/home' element = {
+        <Route path='/' element={<Navigate to="/login" replace />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={
           <PrivateRoute>
-<Home/>
+            <Home />
           </PrivateRoute>
-          }/>
-        <Route path='*' element = {<NotFound/>}/>
+        } />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
